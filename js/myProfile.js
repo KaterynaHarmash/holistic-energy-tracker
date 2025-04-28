49ac23b153308139193f3b3a675ccb683bbbbfce
Kateryna Harmash <ekaterinagarmash1993@gmail.com>
2025-04-28 20:17:26 +0300

    Add table data render

--- js/myProfile.js
+++ js/myProfile.js
@@ -1,4 +1,4 @@
-import { getUserProfile } from "./storage.js";
+import { getEnergyLogs, getUserProfile, renderRecordsLists } from "./storage.js";
 import { darkTheme } from "./themeToggle.js";
 import "./modal.js";
 import { setupAddEnergyLvlValidation } from "./validation.js";
@@ -15,16 +15,7 @@ if (userProfile["dark theme"]) {
 
 setupAddEnergyLvlValidation();
 
-function renderRecordsLists(records){
-  const tableBodyEl = document.querySelector("#recordsDashboard");
+const energyLogs = await getEnergyLogs();
+console.log("logs",energyLogs)
 
-  // <tr class="border-b border-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700">
-  //           <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">Lindsay Walton</td>
-  //           <td class="px-6 py-4">Front-end Developer</td>
-  //           <td class="px-6 py-4">lindsay.walton@example.com</td>
-  //           <td class="px-6 py-4">Member</td>
-  //           <td class="px-6 py-4 text-right">
-  //             <button data-id="" class="font-semibold text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300">Delete</button>
-  //           </td>
-  //         </tr>
-}
+renderRecordsLists(energyLogs);
--- js/storage.js
+++ js/storage.js
@@ -46,13 +46,39 @@ export async function getEnergyLogs(period) {
       dateStart.setDate(dateStart.getDate() - 7);
       break;
     default:
-      dateStart = new Date(0);
+      dateStart = new Date(null);
       break;
   }
 
-  const records = await getUserProfile().energyLogs||[];
-  const filteredRecords = records.filter(rec=>Date.parse(rec.timestamp).getTime()>dateStart.getTime()&&ate.parse(rec.timestamp).getTime()<dateFinish.getTime());
+  const user = await getUserProfile();
+  const filteredRecords = user.energyLogs||[]
+  .filter(rec=>Date.parse(rec.timestamp).getTime()>dateStart.getTime()&&ate.parse(rec.timestamp).getTime()<dateFinish.getTime());
 
 
   return filteredRecords;
+}
+
+export function renderRecordsLists(records){
+  console.log(records)
+  const tableBodyEl = document.querySelector("#recordsDashboard");
+  const tableListMarkup = records.map(({id, timestamp, mood, energyLvl, comment}) => {
+    console.log(timestamp, mood, energyLvl, comment)
+    const date = new Date(timestamp).toLocaleDateString('uk-UA',{
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return `<tr class="border-b border-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700">
+            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">${date}</td>
+            <td class="px-6 py-4">${energyLvl}</td>
+            <td class="px-6 py-4">${mood}</td>
+            <td class="px-6 py-4">${comment}</td>
+            <td class="px-6 py-4 text-right">
+              <button data-id="${id}" class="font-semibold text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300">Delete</button>
+            </td>
+          </tr>`
+    })||[];
+
+    tableBodyEl.insertAdjacentHTML('beforeend', tableListMarkup.join(''));
+  
 }

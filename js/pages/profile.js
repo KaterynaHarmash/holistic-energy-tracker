1ba4802c204d5f33069baf50068bca5e97030790
Kateryna Harmash <ekaterinagarmash1993@gmail.com>
2025-05-11 19:45:15 +0300

    chore: restructure js folder

--- js/pages/profile.js
+++ js/pages/profile.js
@@ -1,8 +1,9 @@
-import { deleteEnergyLog, getEnergyLogs, getUserProfile } from "./storage.js";
-import { darkTheme } from "./themeToggle.js";
-import "./modal.js";
-import { setupAddEnergyLvlValidation } from "./validation.js";
-import { groupEnergyLogsByDay, renderChart } from "./dashboardChart.js";
+import { deleteEnergyLog, getEnergyLogs, getUserProfile } from "../core/storage.js";
+import { darkTheme } from "../components/themeToggle.js";
+import "../components/modal.js";
+import { setupAddEnergyLvlValidation } from "../validation.js";
+import { groupEnergyLogsByDay, renderChart } from "../components/chart.js";
+import { renderRecordsLists } from "../components/tableRender.js";
 
 const userProfile = await getUserProfile();
 if(!userProfile.name){
@@ -24,28 +25,6 @@ const logsFilter = document.querySelector("#filter");
 
 renderRecordsLists(energyLogs);
 const chart = renderChart(energyLogs);
-
-export function renderRecordsLists(records){
-  
-  const tableListMarkup = records.map(({id, timestamp, mood, energyLvl, comment}) => {
-    const date = new Date(timestamp).toLocaleDateString('uk-UA',{
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-    return `<tr class="border-b border-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700">
-            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">${date}</td>
-            <td class="px-6 py-4">${energyLvl}</td>
-            <td class="px-6 py-4">${mood}</td>
-            <td class="px-6 py-4">${comment}</td>
-            <td class="px-6 py-4 text-right">
-              <button data-id="${id}" class="font-semibold text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300 deleteEnergyLogBtn">Delete</button>
-            </td>
-          </tr>`
-    })||[];
-    tableBodyEl.innerHTML = tableListMarkup.join('');
-  
-}
 
 // const deleteBtn = document.querySelector('.deleteEnergyLogBtn');
 tableBodyEl.addEventListener('click',async (e)=>{
